86c7269d99272b878f248d9a6c4f483be0b4fd73
Jan Ivar Korsbakken <j.i.korsbakken@gmail.com>
2024-10-20 18:46:37 +0200

    Copied scripts to listings folder.
    Will be edited, but we commit the original state to be able to track what
    changes were made between the runnable example scripts and the ppt-adapted
    listings.

---
+++ examples/listings_for_ppt/compare_timeseries_ref_data.py
@@ -0,0 +1,83 @@
+# %% [markdown]
+# # Compare model output data with reference data
+#
+# This example shows how to use the `iam-validation` subpackages `.criteria` and
+# `.targets` to compare model output data with timeseries reference data,
+# including detailed comparison for each data point (model, scenario, region, 
+# variable and year) that is present in both the model output and in the
+# reference data.
+
+# %% [markdown]
+# Import the required packages
+
+# %%
+from iam_validation.criteria import TimeseriesRefCriterion
+from iam_validation.targets import CriterionTargetRange
+
+from pathlib import Path
+
+import pandas as pd
+import pyam
+
+
+# %% [markdown]
+# Load your reference timeseries data for one or more varaibles (e.g, data to be
+# used for harmonization), which needs to be prepared in IAMC format and
+# loadable with `pyam`. Replace `referenc_file` below with the path to your
+# reference timeseries data, as a string or `pathlib.Path` object.
+
+# %%
+reference_file: Path|str = Path('.') / 'my_reference_data.xlsx'
+ref_data = pyam.IamDataFrame(reference_file)
+
+
+# %% [markdown]
+# Create a timeseries reference object that compars model results to the
+# reference data by taking the ratio, and define an allowed target range of
+# +/- 5%. The `criterion_name` can be any string, bu should be recognizable (can
+# be used by output functions later). Comparisons are here made for each
+# variable, region and year that are present in both the model output and the
+# reference data.
+
+# %%
+timeseries_ref = TimeseriesRefCriterion(
+    criterion_name='Reference data ratio comparison',
+    reference=ref_data,
+    comparison_function='ratio',
+    broadcast_dims=('model', 'scenario'),
+)
+target_range = CriterionTargetRange(
+    criterion=timeseries_ref,
+    target=1.0,
+    range=(0.95, 1.05),
+)
+
+
+# %% [markdown]
+# Load the model data that you want to compare to the reference data, from an
+# IAMC-formatted Excel or CSV file.
+
+# %%
+model_file: Path|str = Path('.') / 'my_model_output.xlsx'
+model_df = pyam.IamDataFrame(model_file)
+
+
+# %% [markdown]
+# Get a pandas.Series with the ratios of model data relative reference data
+
+# %%
+# TODO: Consider whether to make `compare` return a `TargetComparison` instance
+# or something like that. Or perhaps make it cache results, or even remember the
+# last df that it was called on. Or just cache the results, but still require
+# user to pass in the desired IamDataFrame with model data each time, in order
+# to make explicit what data is being compared.
+comparison_ratios: pd.Series = timeseries_ref.get_values(model_df)
+
+
+# %% [markdown]
+# Get a pandas.Series with True/False for what data points are inside the
+# allowed target range.
+
+# %%
+in_range_statuses: pd.Series = target_range.get_in_range(model_df)
+
---
+++ examples/listings_for_ppt/find_invalid_names.py
@@ -0,0 +1,82 @@
+# %% [markdown]
+# # Find invalid names and variable/unit combinations
+#
+# This example shows how to use the `iam_validation.nomenclature` package to
+# detect invalid names and variable/unit combinations in your IAM output.
+#
+# The model output must be in a file that can be read into a `pyam.IamDataFrame`
+# object by the `pyam` package, i.e., an Excel or CSV file in IAMC format.
+
+# %% [markdown]
+# Import required classes from the `iam_validation.nomenclature` package,
+# and required modules/classes from `pandas` and `pyam`.
+
+# %%
+from iam_validation.nomenclature import (
+    NomeclatureDefs,
+    MergedDefs,
+    COMMON_DEFINITIONS_URL,
+)
+
+from pathlib import Path
+
+import pandas as pd
+import pyam
+
+
+# %% [markdown]
+# Get variable and region names from `common_definitions`, but *not* region
+# aggregation mappings from model-native to common regions. We will use mappings
+# from our own project definitions instead.
+
+# %%
+common_defs = NomeclatureDefs.from_url(COMMON_DEFINITIONS_URL,
+                                       region_mappings=False)
+
+# %% [markdown]
+# Get project-specific names for all dimensions, and region-mappings from custom
+# project definitions
+
+# %%
+project_defs = NomenclatureDefs.from_url(
+    repo_url,
+    dimensions=['model', 'scenario', 'region', 'variable'],
+)
+
+# %% [markdown]
+# Merge the definitions. Let project-specific definitions override
+# `common-definitions` where they overlap.
+
+# %%
+merged_defs: MergedDefs = common_defs.update(project_defs)
+
+
+# %% [markdown]
+# Now load a file with model results. Set `results_file` below to a path to your
+# model results, as an IAMC-formatted Excel or CSV file. Can be either a string
+# or a `pathlib.Path` object.
+
+# %%
+model_file: Path|str = Path('.') / 'my_model_output.xlsx'
+model_df = pyam.IamDataFrame(model_file)
+
+
+# %% [markdown]
+# Get invalid names, including recognized model-native ones. Returns a dict with
+# dimension names as keys, and invalid names for each dimension as a list, or
+# as a DataFrame of invlid model/region-name pairs when recognizing unammped
+# model-native region names.
+
+# %%
+invalid_names: dict[str, list[str]|pd.DataFrame] = \
+    merged_defs.get_invalid_names(model_df, raw_model_regions=True)
+
+
+# %% [markdown]
+# Get invalid unit/variable combos. Returns a DataFrame, where the index is
+# known variables that have unrecognized units, with one column with lists of
+# the unrecognized units for each variable, and one column with lists of the
+# valid unit names for that variable.
+
+# %%
+invalid_units: pd.DataFrame = merged_defs.get_invalid_variable_units(model_df)
