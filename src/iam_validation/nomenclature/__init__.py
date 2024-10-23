0429d35a6b528eff79f9b2e2e6a6220726eed69b
Jan Ivar Korsbakken <j.i.korsbakken@gmail.com>
2024-10-23 11:06:01 +0200

    Changes in nomenclature.validation to account for refactoring.

--- src/iam_validation/nomenclature/__init__.py
+++ src/iam_validation/nomenclature/__init__.py
@@ -1,16 +1,17 @@
 """Package to read and use definitions and mappings for IAM COMPACT."""
 import functools
-
-# Import _region_adjustments to make sure region attributes are adjusted as
-# needed before the definitions are loaded.
-# Change: Don't adjust for regions yet. Since the ISO3 code check in
-# `nomenclature.RegionCode` happens directly against the ISO2 codes in
-# `pycountry.countries`, making adjustments here won't be enough. Need to wait
-# until the `nomenclature` code is changed, if ever.
-# from . import _region_adjustments
+import typing as tp
 
 from .. import type_helpers
 
 from . import defs
 from . import mapping
 from . import validation
+
+from .defs import NomenclatureDefs
+
+
+
+COMMON_DEFINITIONS_URL: tp.Final[str] \
+    = 'https://github.com/IAMconsortium/common-definitions.git'
+"""The URL of the `common-definitions` repository."""
